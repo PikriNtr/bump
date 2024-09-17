c7abd9904fc19caad391e91a6cb107bb8ec9ee57
Muh Fikri Haikal Adudu <fikrihaikal2101@gmail.com>
2024-09-17 10:24:56 +0800

    Add files via upload

---
+++ flowchart.py
@@ -0,0 +1,90 @@
+
+
+
+
+#i = 0
+#while i <= 21:
+#    print(i)
+#    i += 2
+
+#for i in range (1, 21):
+#   print(i, end="")
+#   if i == 15:
+#       print(".")
+#       continue
+#   print(". PikriNtr")
+
+
+a = 20
+b = 1200
+i = a
+while i <= b:
+    
+    print(i, ". Fikri", sep="")
+    i += 1
+
+#i = 1
+#while i <= 15:
+#    if i <= 9:
+#        print(" ", end="")
+#    print(i, ". Fikri", sep="")
+#    i += 1
+
+
+#i = 1
+#while i <= 20:
+#    print(i, end="")
+#    i += 1
+#    if i == 16:
+#        print(".")
+#        continue
+#        break
+#    print(". Fikri")
+    
+
+
+#i = 1
+#while i < 21:
+#    if i == 8:
+#        i += 7
+#    print(i)
+#    i += 1
+
+
+#for i in range(1, 21):
+#    if i >= 1 and i <= 7 or i >= 15 and i <= 20:
+# //optional    if 1 <= i and i <= 7 or 15 <= i and i <= 20:
+#       print(i)
+
+
+
+#for i in range(1, 21, 2):
+#    print(i)
+
+
+
+#a = '2.5 '
+#b = int(float(a))
+#print(b)
+
+#a = input()
+#print(int(float(a)) *4)
+
+#print("'\\\\\'")
+#print('\'\\\\\'')
+
+
+#import math
+
+#a = 3.0
+#b = 4.0
+#c =math.sqrt(math.pow(a, 2) + math.pow(b, 2))
+#print('a = ',a,'\n', 'b = ',b,'\n','c = ', c, sep='')
+
+
+#a = False
+#b = True
+
+#xor = a and not b or not a and b
+
+#print(a , 'xor', b, '=', xor)
