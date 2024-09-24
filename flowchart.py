437c8580386c22e749d6a70080700d66a8514227
Muh Fikri Haikal Adudu <fikrihaikal2101@gmail.com>
2024-09-24 11:15:43 +0800

    Add files via upload

--- flowchart.py
+++ flowchart.py
@@ -1,6 +1,31 @@
+a=[]
+for i in range(10):
+    a.append(i)
+print(a)
+
+#append/insert
 
 
+#n = [12, 5, 20, 10, 15]
+#print(n)
+#
+#for i in range(0, len(n)-1):
+#    for j in range(i + 1, len(n)):
+#        if n[j] < n[i]:
+#            n[j], n[i] = n[i], n[j]
+#print(n)
 
+#numbers = [1, 2, 3, 2, 1]
+#print("list originalnya:", numbers) 
+#numbers.insert(-1, 4)
+#print('numbers =', numbers)
+#total = 0
+#index = 0
+#while index < len(numbers):
+#    total += numbers[index]
+#    index += 1
+#    
+#print('totalnya =', total)
 
 #i = 0
 #while i <= 21:
@@ -14,11 +39,11 @@
 #       continue
 #   print(". PikriNtr")
 
-i = 111
-for i in range(1, 100):
-    print(i)
-else:
-    print("else:", i)
+#i = 111
+#for i in range(1, 100):
+#    print(i)
+#else:
+#    print("else:", i)
 
 
 
